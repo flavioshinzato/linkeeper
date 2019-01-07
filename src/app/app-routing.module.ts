d25534e970e99f9b9e75567bc196f80ff7f43be6
Flávio Augusto Müller <f.a.m.shinzato@gmail.com>
2019-01-06 22:04:34 -0300

    Router corrigido.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,7 +8,7 @@ import { LoginComponent } from './login/login.component';
 const routes: Routes = [
   {
     path: '',
-    component: HomeComponent
+    component: LoginComponent
   },
   {
     path: 'home',
