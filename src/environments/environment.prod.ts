a44db6d0c7cf76f992f8748454df63beb2cf7207
Flávio Augusto Müller <f.a.m.shinzato@gmail.com>
2019-01-06 23:40:09 -0300

    Adicionando configuração do firebase no enviroment.prod.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyA7KpWgRGHiN7nMOonMZPrUozF9i5Ka330",
+    authDomain: "linkeeper-b6955.firebaseapp.com",
+    databaseURL: "https://linkeeper-b6955.firebaseio.com",
+    projectId: "linkeeper-b6955",
+    storageBucket: "linkeeper-b6955.appspot.com",
+    messagingSenderId: "861368050076"
+  }
 };
